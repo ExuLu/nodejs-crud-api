2e1d41a1605e34637751279026066adc65b9a785
Alena Sharai <exultantislupus@gmail.com>
2024-02-10 18:09:00 +0400

    feat: add users url

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import { createServer } from 'http';
+import users from './utils/users.js';
 
 const port = 3000;
 const server = createServer((req, res) => {
-  res.write('Hello');
-  res.end();
+  const { url, method } = req;
+  if (url === '/users' && method === 'GET') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(users));
+  }
 });
 
 server.listen(port, () => console.log(`Server listened in port ${port}`));
---
+++ utils/users.js
@@ -0,0 +1,15 @@
+const users = [
+  {
+    id: 1,
+    userName: 'littlepony',
+  },
+  {
+    id: 2,
+    userName: 'teddybear',
+  },
+  {
+    id: 3,
+    userName: 'magicunicorn',
+  },
+];
+export default users;
