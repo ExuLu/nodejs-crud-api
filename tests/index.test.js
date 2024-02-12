a6c54a6f1bb4395f10cd4b7ed2c8c31b20263248
Alena Sharai <exultantislupus@gmail.com>
2024-02-12 20:55:32 +0400

    feat: add test for non-existing user

--- tests/index.test.js
+++ tests/index.test.js
@@ -48,4 +48,23 @@ describe('API tests', () => {
     expect(result.status).toBe(200);
     expect(result.body).toEqual({ ...updatedUser, id });
   });
+
+  test('Delete user with DELETE method', async () => {
+    const newUser = await request(server).post(url).send(user);
+    const id = newUser.body.id;
+    const userURL = `${url}/${id}`;
+    const result = await request(server).delete(userURL);
+
+    expect(result.status).toBe(204);
+  });
+
+  test('Should return error for non-existing user', async () => {
+    const newUser = await request(server).post(url).send(user);
+    const id = newUser.body.id;
+    const userURL = `${url}/${id}`;
+    await request(server).delete(userURL);
+    const getUser = await request(server).get(userURL);
+
+    expect(getUser.status).toBe(404);
+  });
 });
