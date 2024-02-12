1155435c9f926797461ac72c2cddfed5dcee99c0
Alena Sharai <exultantislupus@gmail.com>
2024-02-12 11:14:33 +0400

    feat: create get method for get user by id

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ const server = createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify(users));
   }
+  if (url.includes('/users/') && method === 'GET') {
+    const id = url.slice(url.lastIndexOf('/') + 1);
+    const user = users.find((use) => use.id === id);
+    if (user) {
+      res.writeHead(200, { 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(user));
+    } else {
+      res.writeHead(404, { 'Content-Type': 'text/plain' });
+      res.end('We do not have user with such id');
+    }
+  }
 });
 
 server.listen(port, () => console.log(`Server listened in port ${port}`));
--- utils/users.js
+++ utils/users.js
@@ -1,4 +1,4 @@
-const { v4: uuidv4 } = require('uuid');
+import { v4 as uuidv4 } from 'uuid';
 
 const users = [
   {
@@ -6,6 +6,12 @@ const users = [
     username: 'littlepony',
     age: 19,
     hobbies: ['Collect butterflies', 'Play in rock band'],
+  },
+  {
+    id: uuidv4(),
+    username: 'littlefoxy',
+    age: 25,
+    hobbies: ['Hunting', 'Eating'],
   },
 ];
 export default users;
